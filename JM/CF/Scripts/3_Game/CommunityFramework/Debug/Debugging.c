497881ee20cf775f708b93d2d1740cb0e778189e
Jacob-Mango <mangosprograms@gmail.com>
2019-05-16 16:56:30 +1000

    Added debugging file loading

--- JM/CF/Scripts/3_Game/CommunityFramework/Debug/Debugging.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/Debug/Debugging.c
@@ -1,8 +1,16 @@
 class Debugging
 {
-    bool JM_CF_Mods = true;
-	bool JM_CF_RPC = true;
-	bool JM_CF_Credits = true;
+	[NonSerialized()]
+	string m_FileName;
+	
+    bool JM_CF_Mods;
+	bool JM_CF_RPC;
+	bool JM_CF_Credits;
+	
+	private void Debugging()
+	{
+		m_FileName = "$profile:Debugging.json";
+	}
 	
 	void Log( string text, string type )
 	{
@@ -15,6 +23,34 @@ class Debugging
 			Print( "" + text );
 		}
 	}
+	
+	static ref Debugging Load()
+	{
+		ref Debugging settings = new Debugging();
+
+		if ( FileExist( m_FileName ) )
+		{
+			JsonFileLoader<Debugging>.JsonLoadFile( m_FileName, settings );
+		} else {
+			settings.Defaults();
+			settings.Save();
+		}
+
+		return settings;
+	}
+
+	void Save()
+	{
+		JsonFileLoader<Debugging>.JsonSaveFile( m_FileName, this );
+	}
+	
+	void Defaults()
+	{
+		// TODO: Change to false
+	    JM_CF_Mods = true;
+		JM_CF_RPC = true;
+		JM_CF_Credits = true;
+	}
 }
 
 static ref Debugging g_Debugging;
@@ -23,7 +59,7 @@ static ref Debugging GetDebugging()
 {
     if ( !g_Debugging )
     {
-        g_Debugging = new ref Debugging;
+        g_Debugging = Debugging.Load();
     }
 
     return g_Debugging;
