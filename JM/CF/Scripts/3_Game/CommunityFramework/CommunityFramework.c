493e59a769dfd5487376bad84c52df4e67a9d4cb
Jacob-Mango <mangosprograms@gmail.com>
2019-10-04 18:10:38 +1000

    Added CF_DumpWidgets

--- JM/CF/Scripts/3_Game/CommunityFramework/CommunityFramework.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/CommunityFramework.c
@@ -21,3 +21,42 @@ static bool IsMissionOffline()
 
     return GetGame().IsServer() && !GetGame().IsMultiplayer();
 }
+
+static void CF_DumpWidgets( Widget root, int tabs = 0 )
+{
+	if ( !root )
+	{
+		return;
+	}
+	
+	string tmp;
+	for ( int i = 0; i < tabs; i++ )
+	{
+		tmp += "  ";
+	}
+	
+	string invisible = "";
+	
+	if ( !root.IsVisibleHierarchy() )
+	{
+		invisible = "[invisible]";
+	}
+	
+	Print( tmp + "- " + root.GetName() + " (" + root.GetTypeName() + ") " + invisible );
+	
+	bool collapse = false;
+	if ( root.GetChildren() )
+	{
+		collapse = true;
+		Print( tmp + "{" );
+	}
+	
+	CF_DumpWidgets( root.GetChildren(), tabs + 1 );
+		
+	if ( collapse )
+	{
+		Print( tmp + "}" );
+	}
+			
+	CF_DumpWidgets( root.GetSibling(), tabs );
+}
