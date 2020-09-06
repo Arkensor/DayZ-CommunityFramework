81ed84231739ce465acbb3b2425d45874d7cffde
InclementDeath <tylerpaul2329@gmail.com>
2020-09-06 17:26:57 -0400

    migrated Collection args

--- JM/CF/Scripts/3_Game/CommunityFramework/MVC/Types.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/MVC/Types.c
@@ -1,4 +1,29 @@
 
+enum NotifyCollectionChangedAction {
+	Add,
+	Remove,
+	Set,
+	Move,
+	Clear
+};
+
+
+// Event Args for Collection Changed
+// 0: Observable that was changed
+// 1: Collection Changed Action
+// 2: Changed Index
+// 3: Changed Value
+typedef Param4<Class, int, int, ref Param> CollectionChangedEventArgs;
+
+
+
+// Event Args for Property Changed
+// 0: Name of property changed
+// 1: New property value
+typedef Param2<string, Class> PropertyChangedEventArgs;
+
+
+
 // 0: Property Name
 // 1: Proprety Type
 class PropertyHashMap: ref map<string, typename>
