af62a89c2e95e8beaa37e663dae0ecb918981b56
InclementDeath <tylerpaul2329@gmail.com>
2020-09-16 14:10:43 -0400

    Added CanExecuteChanged, and RelayCommand documentation

--- JM/CF/Scripts/3_Game/CommunityFramework/MVC/RelayCommand.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/MVC/RelayCommand.c
@@ -3,10 +3,31 @@
 
 // Abstract Class
 class RelayCommand
-{		
+{
+	private bool m_CanExecute = true;
+	
+	
+	/* Getters and Setters */
+	
+	// returns bool defining whether or not the Execute can be called
+	bool CanExecute() {
+		return m_CanExecute;
+	}
+	
+	// sets whether or not RelayCommand can be executed
+	// depreciate if BI ever adds property Getters and Setters
+	void SetCanExecute(bool state) {
+		m_CanExecute = state;
+		CanExecuteChanged(m_CanExecute);
+	}
+	
+	
+	/* Abstract Methods */
+	
+	// called when type is Clicked, Selected, or Changed
 	void Execute(RelayCommandArgs args);
 	
-	bool CanExecute() 
-		return true;
+	// Abstract function called when execution ability is changed
+	void CanExecuteChanged(bool state);
 }
 
