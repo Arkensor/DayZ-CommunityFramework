40657ecba4ce1fc2f07aaf83ae3288a8e80afa61
InclementDeath <tylerpaul2329@gmail.com>
2020-09-16 14:36:58 -0400

    Added Observable set and more documentation

---
+++ JM/CF/Scripts/3_Game/CommunityFramework/MVC/Classes/EventArgs.c
@@ -0,0 +1,116 @@
+
+// Event Args for Collection Changed
+// 0: Collection that was changed
+// 1: Collection Changed Action
+// 2: Collection Changed Index
+// 3: Collection Changed Value
+class CollectionChangedEventArgs: Param4<Class, int, int, ref Param>
+{
+	Observable GetCollection() {
+		return Observable.Cast(param1);
+	}
+	
+	NotifyCollectionChangedAction GetChangedAction() {
+		return param2;
+	}
+	
+	int GetChangedIndex() {
+		return param3;
+	}
+	
+	Param GetChangedValue() {
+		return param4;
+	}
+}
+
+
+
+// Event Args for Property Changed
+// 0: Name of property changed
+// 1: New property value
+class PropertyChangedEventArgs: Param2<string, Class>
+{
+	string GetPropertyName() {
+		return param1;
+	}
+	
+	Class GetPropertyValue() {
+		return param2;
+	}
+}
+
+
+// 0: Source Widget
+// 1: Mouse Button
+// 2: Button State
+class ButtonCommandArgs: Param3<ButtonWidget, int, bool>
+{
+	ButtonWidget GetButtonWidget() { 
+		return param1; 
+	}
+	
+	int GetMouseButton() {
+		return param2;
+	}
+	
+	bool GetButtonState() {
+		return param3;
+	}
+}
+
+
+// 0: Source Widget
+// 1: CheckBox State
+class CheckBoxCommandArgs: Param2<CheckBoxWidget, bool>
+{
+	CheckBoxWidget GetCheckBoxWidget() {
+		return param1;
+	}
+	
+	bool GetCheckBoxState() {
+		return param2;
+	}
+}
+
+// 0: Source Widget
+// 1: Selected Item
+class XComboBoxCommandArgs: Param2<XComboBoxWidget, int>
+{
+	XComboBoxWidget GetComboBoxWidget() {
+		return param1;
+	}
+	
+	int GetSelectedIndex() {
+		return param2;
+	}
+}
+
+// 0: Source Widget
+// 1: Selected Widget
+class WrapSpacerCommandArgs: Param2<WrapSpacerWidget, Widget>
+{
+	WrapSpacerWidget GetWrapSpacerWidget() {
+		return param1;
+	}
+	
+	Widget GetSelectedWidget() {
+		return param2;
+	}
+}
+
+// 0: Call Context
+// 1: Command Args (see above)
+class RelayCommandArgs
+{
+	Class Context;
+	Param CommandArgs;
+	typename CommandArgsType;
+	
+	void RelayCommandArgs(Class context, Param command_args)
+	{
+		Context = context;
+		CommandArgs = command_args;
+		CommandArgsType = CommandArgs.Type();
+	}
+	
+}
--- JM/CF/Scripts/3_Game/CommunityFramework/MVC/Classes/Types.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/MVC/Classes/Types.c
@@ -1,55 +1,4 @@
 
-enum NotifyCollectionChangedAction {
-	Add,
-	Remove,
-	Set,
-	Move,
-	Clear
-};
-
-
-// Event Args for Collection Changed
-// 0: Collection that was changed
-// 1: Collection Changed Action
-// 2: Collection Changed Index
-// 3: Collection Changed Value
-class CollectionChangedEventArgs: Param4<Class, int, int, ref Param>
-{
-	Observable GetCollection() {
-		return Observable.Cast(param1);
-	}
-	
-	NotifyCollectionChangedAction GetChangedAction() {
-		return param2;
-	}
-	
-	int GetChangedIndex() {
-		return param3;
-	}
-	
-	Param GetChangedValue() {
-		return param4;
-	}
-}
-
-
-
-// Event Args for Property Changed
-// 0: Name of property changed
-// 1: New property value
-class PropertyChangedEventArgs: Param2<string, Class>
-{
-	string GetPropertyName() {
-		return param1;
-	}
-	
-	Class GetPropertyValue() {
-		return param2;
-	}
-}
-
-
-
 // 0: Property Name
 // 1: Proprety Type
 class PropertyHashMap: ref map<string, typename>
@@ -69,40 +18,18 @@ class PropertyHashMap: ref map<string, typename>
 // 0: Property Name
 // 1: View Binding
 typedef ref set<ref ViewBinding> ViewBindingSet;
-class ViewBindingHashMap: ref map<Widget, ref ViewBindingSet>
-{
-	void DebugPrint()
-	{
-		foreach (Widget w, ViewBindingSet view_set: this) {
-			PrintFormat("[%1]:", w.GetName()); 
-			foreach (ViewBinding view: view_set) {
-				PrintFormat("    %1", view.GetLayoutRoot().GetName());
-			}
-		}		
-	}
-	
-	void InsertView(ViewBinding view)
-	{
-		Widget key = view.GetLayoutRoot();
-		ViewBindingSet view_set = Get(key);
-		if (!view_set) {
-			view_set = new ViewBindingSet();
-			view_set.Insert(view);
-			Insert(key, view_set);
-		} else {
-			view_set.Insert(view);
-		}		
-	}
-}
 
+
+// 0: Property Name
+// 1: View Binding Set
 class DataBindingHashMap: ref map<string, ref ViewBindingSet>
 {
 	void DebugPrint()
 	{
 		foreach (string name, ViewBindingSet view_set: this) {
-			PrintFormat("[%1]:", name); 
+			MVC.Log("[%1]:", name);
 			foreach (ViewBinding view: view_set) {
-				PrintFormat("    %1", view.GetLayoutRoot().GetName());
+				MVC.Log("    %1", view.GetLayoutRoot().GetName());
 			}
 		}
 	}
@@ -121,82 +48,6 @@ class DataBindingHashMap: ref map<string, ref ViewBindingSet>
 	}
 }
 
-
-
-// 0: Source Widget
-// 1: Mouse Button
-// 2: Button State
-class ButtonCommandArgs: Param3<ButtonWidget, int, bool>
-{
-	ButtonWidget GetButtonWidget() { 
-		return param1; 
-	}
-	
-	int GetMouseButton() {
-		return param2;
-	}
-	
-	bool GetButtonState() {
-		return param3;
-	}
-}
-
-
-// 0: Source Widget
-// 1: CheckBox State
-class CheckBoxCommandArgs: Param2<CheckBoxWidget, bool>
-{
-	CheckBoxWidget GetCheckBoxWidget() {
-		return param1;
-	}
-	
-	bool GetCheckBoxState() {
-		return param2;
-	}
-}
-
-// 0: Source Widget
-// 1: Selected Item
-class XComboBoxCommandArgs: Param2<XComboBoxWidget, int>
-{
-	XComboBoxWidget GetComboBoxWidget() {
-		return param1;
-	}
-	
-	int GetSelectedIndex() {
-		return param2;
-	}
-}
-
-// 0: Source Widget
-// 1: Selected Widget
-class WrapSpacerCommandArgs: Param2<WrapSpacerWidget, Widget>
-{
-	WrapSpacerWidget GetWrapSpacerWidget() {
-		return param1;
-	}
-	
-	Widget GetSelectedWidget() {
-		return param2;
-	}
-}
-
-// 0: Call Context
-// 1: Command Args (see above)
-class RelayCommandArgs
-{
-	Class Context;
-	Param CommandArgs;
-	typename CommandArgsType;
-	
-	void RelayCommandArgs(Class context, Param command_args)
-	{
-		Context = context;
-		CommandArgs = command_args;
-		CommandArgsType = CommandArgs.Type();
-	}
-	
-}
 
 
 enum WidgetEventType { // unused
