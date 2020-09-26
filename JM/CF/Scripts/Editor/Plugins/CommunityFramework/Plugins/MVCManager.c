55a725958c4b91ce4f851de305baa40cf73c133b
InclementDeath <tylerpaul2329@gmail.com>
2020-09-26 15:36:26 -0400

    Added additional control

--- JM/CF/Scripts/Editor/Plugins/CommunityFramework/Plugins/MVCManager.c
+++ JM/CF/Scripts/Editor/Plugins/CommunityFramework/Plugins/MVCManager.c
@@ -4,10 +4,22 @@ class MVCManager: WorkbenchPlugin
 	[Attribute("", "combobox", "", "", ParamEnumArray.FromEnum(LogLevel))]
 	int Log_Level;
 	
-	override void Configure()
+	override void Run()
 	{
+		Log_Level = MVCLogLevel;
 		Workbench.ScriptDialog("Edit MVC Settings", "", this);
+	}
+	
+	[ButtonAttribute("OK")]
+	void DialogOk()
+	{
 		MVCLogLevel = Log_Level;
+	}
+		
+	[ButtonAttribute("Cancel")]
+	void DialogCancel()
+	{
+		
 	}
 }
  
