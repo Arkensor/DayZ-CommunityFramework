abb2a7374a3000a02df197162a2f8d99dea9a2b0
InclementDeath <tylerpaul2329@gmail.com>
2020-09-26 15:12:06 -0400

    Added additional conditionals to logging

--- JM/CF/Scripts/3_Game/CommunityFramework/MVC/ScriptedViewBase.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/MVC/ScriptedViewBase.c
@@ -6,8 +6,6 @@ class ScriptedViewBase: ScriptedWidgetEventHandler
 	// Enable verbose logging
 	reference bool Debug_Logging;
 	
-	bool Trace_Logs = true;
-	
 	void ScriptedViewBase()
 	{
 		Log(Type().ToString());
@@ -24,13 +22,13 @@ class ScriptedViewBase: ScriptedWidgetEventHandler
 	
 	void Trace(string message, string param1 = "", string param2 = "", string param3 = "", string param4 = "", string param5 = "", string param6 = "", string param7 = "", string param8 = "", string param9 = "")
 	{
-		if (Debug_Logging && Trace_Logs)
+		if (Debug_Logging || MVCLogLevel >= LogLevel.TRACE)
 			PrintFormat("[Trace] %1 - %2 ", Type(), string.Format(message, param1, param2, param3, param4, param5, param6, param7, param8, param9));
 	}
 	
 	void Log(string message, string param1 = "", string param2 = "", string param3 = "", string param4 = "", string param5 = "", string param6 = "", string param7 = "", string param8 = "", string param9 = "")
 	{
-		if (Debug_Logging)
+		if (Debug_Logging || MVCLogLevel >= LogLevel.INFO)
 			PrintFormat("[Log] %1 - %2", Type(), string.Format(message, param1, param2, param3, param4, param5, param6, param7, param8, param9));
 	}
 	
