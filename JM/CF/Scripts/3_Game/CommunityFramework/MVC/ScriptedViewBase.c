567c588c1ab5573369c57c55573ceda2123440ae
InclementDeath <tylerpaul2329@gmail.com>
2020-09-28 01:24:06 -0400

    Update ScriptedViewBase.c
    - Re-added Trace logs

--- JM/CF/Scripts/3_Game/CommunityFramework/MVC/ScriptedViewBase.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/MVC/ScriptedViewBase.c
@@ -46,7 +46,7 @@ class ScriptedViewBase: ScriptedWidgetEventHandler
 	void Trace(string message, string param1 = "", string param2 = "", string param3 = "", string param4 = "", string param5 = "", string param6 = "", string param7 = "", string param8 = "", string param9 = "")
 	{
 //#ifdef COMPONENT_SYSTEM
-		if (Debug_Logging && false) // || MVCLogLevel >= LogLevel.TRACE
+		if (Debug_Logging) // || MVCLogLevel >= LogLevel.TRACE
 			PrintFormat("[Trace] %1 - %2 ", Type(), string.Format(message, param1, param2, param3, param4, param5, param6, param7, param8, param9));
 //#endif
 	}
