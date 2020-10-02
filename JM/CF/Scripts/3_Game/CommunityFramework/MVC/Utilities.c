4dd7d98a8b350af89de4b1490be910936105149b
InclementDeath <tylerpaul2329@gmail.com>
2020-10-01 21:36:34 -0400

    Update Utilities.c
    - Added Documentation to LoadViewProperties
    - Added better conditioning inside of LoadViewProperties

--- JM/CF/Scripts/3_Game/CommunityFramework/MVC/Utilities.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/MVC/Utilities.c
@@ -1,19 +1,43 @@
+
+
+// Loads .layout file Widgets into Properties of context (when they are the same name)
+/*
+
+Example:
+
+.layout file:
+MenuBarRoot		FrameWidget
+	MenuBarFile   	ButtonWidget
+		MenuBarFileLabel	TextWidget
+
+
+.c file:
+class TestClass
+{
+	ButtonWidget MenuBarFile; //<-- these properties will be assigned
+	private TextWidget MenuBarFileLabel;
+}
+
+*/
 static void LoadViewProperties(Class context, PropertyTypeHashMap property_map, Widget root_widget)
 {
 	foreach (string property_name, typename property_type: property_map) {
 		
-		if (property_type.IsInherited(Widget)) {
-			Widget target = root_widget.FindAnyWidget(property_name);
-			
-			// fixes bug that breaks everything
-			if (target && root_widget.GetName() != property_name) {
-				EnScript.SetClassVar(context, property_name, 0, target);
-			}
-			
-			// Allows you to define the layout root aswell within it
-			if (!target && root_widget.GetName() == property_name) {
-				EnScript.SetClassVar(context, property_name, 0, root_widget);
-			}
+		if (!property_type.IsInherited(Widget)) continue;
+		
+		Widget target = root_widget.FindAnyWidget(property_name);
+					
+		// fixes bug that breaks everything
+		if (target && root_widget.GetName() != property_name) {
+			EnScript.SetClassVar(context, property_name, 0, target);
+			continue;
 		}
+		
+		// Allows you to define the layout root aswell within it
+		if (!target && root_widget.GetName() == property_name) {
+			EnScript.SetClassVar(context, property_name, 0, root_widget);
+			continue;
+		}
+		
 	}
 }
