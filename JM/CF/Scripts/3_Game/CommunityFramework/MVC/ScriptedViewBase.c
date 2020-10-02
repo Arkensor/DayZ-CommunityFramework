a58ed0aad02d697eb0cb6df836c715692acbdbb5
InclementDeath <tylerpaul2329@gmail.com>
2020-10-02 08:24:29 -0400

    Update ScriptedViewBase.c

--- JM/CF/Scripts/3_Game/CommunityFramework/MVC/ScriptedViewBase.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/MVC/ScriptedViewBase.c
@@ -86,7 +86,7 @@ class ScriptedViewBase: Managed
 	void Trace(string message, string param1 = "", string param2 = "", string param3 = "", string param4 = "", string param5 = "", string param6 = "", string param7 = "", string param8 = "", string param9 = "")
 	{
 //#ifdef COMPONENT_SYSTEM
-		//if (Debug_Logging) // || MVCLogLevel >= LogLevel.TRACE
+		if (Debug_Logging) // || MVCLogLevel >= LogLevel.TRACE
 			PrintFormat("[Trace] %1 - %2 ", Type(), string.Format(message, param1, param2, param3, param4, param5, param6, param7, param8, param9));
 //#endif
 	}
@@ -95,7 +95,7 @@ class ScriptedViewBase: Managed
 	void Log(string message, string param1 = "", string param2 = "", string param3 = "", string param4 = "", string param5 = "", string param6 = "", string param7 = "", string param8 = "", string param9 = "")
 	{
 //#ifdef COMPONENT_SYSTEM
-		//if (Debug_Logging) // || MVCLogLevel >= LogLevel.INFO
+		if (Debug_Logging) // || MVCLogLevel >= LogLevel.INFO
 			PrintFormat("[Log] %1 - %2", Type(), string.Format(message, param1, param2, param3, param4, param5, param6, param7, param8, param9));
 //#endif
 	}
