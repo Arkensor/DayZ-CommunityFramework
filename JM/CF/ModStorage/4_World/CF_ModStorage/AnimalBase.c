c7e060eeea173f96816f9f46337ae57f004671e7
Jacob-Mango <mangosprograms@gmail.com>
2020-11-24 23:26:46 +1100

    Added missing classes to ModStorage

---
+++ JM/CF/ModStorage/4_World/CF_ModStorage/AnimalBase.c
@@ -0,0 +1,136 @@
+modded class AnimalBase
+{
+	override void OnStoreSave( ParamsWriteContext ctx )
+	{
+		super.OnStoreSave( ctx );
+
+		ctx.Write( ModStorage.VERSION );
+	
+		array< ref ModStructure > mods = ModLoader.GetMods();
+
+		int count = mods.Count();
+
+		ctx.Write( count );
+
+		for ( int i = 0; i < count; i++ )
+		{
+			ModStorage store = new ModStorage( mods[ i ] );
+
+			OnModStoreSave( store, mods[ i ].GetName() );
+
+			store.Save( this, ctx );
+		}
+	}
+
+	override bool OnStoreLoad( ParamsReadContext ctx, int version )
+	{
+		if ( !super.OnStoreLoad( ctx, version ) )
+			return false;
+
+		// If the persistence file is before 1.10, cf data doesn't exist
+		if ( version < 116 )
+			return true;
+
+		int cf_version;
+		if ( !ctx.Read( cf_version ) )
+			return false;
+
+		int count;
+		if ( !ctx.Read( count ) )
+			return false;
+
+		for ( int i = 0; i < count; i++ )
+		{
+			string modName;
+			if ( !ctx.Read( modName ) )
+				return false;
+
+			ModStorage store = new ModStorage( ModLoader.Get( modName ) );
+
+			if ( !store.Load( this, ctx, cf_version ) )
+			{
+				Error( "Failed reading " + GetType() + " for mod '" + modName + "'!" );
+				return false;
+			}
+
+			if ( store.GetMod() && store.GetVersion() > 0 && !OnModStoreLoad( store, modName ) )
+			{
+				Error( "Failed loading " + GetType() + " for mod '" + modName + "'!" );
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param storage	Where the data is written to, only use public methods 'void Write...(value)`
+	 * @param modName	The name of the mod from CfgMods class to check against
+	 * 
+	 * @code
+	modded class Sedan_02 // extends from AnimalBase
+	{
+		override void OnModStoreSave( ModStorage storage, string modName )
+		{
+			//! Always call super at the start.
+			super.OnModStoreSave( storage, modName );
+
+			if ( modName != "JM_CommunityFramework" )
+				return; //! Early exit
+
+			storage.WriteVector( GetOrientation() );
+			storage.WriteInt( 6 );
+
+			//! The version of the mod is set in 'ModStructure::OnLoad', using 'SetStorageVersion'
+			if ( storage.GetVersion() > 1 ) //! this check is redudant
+			{
+				storage.WriteString( "ThisVariableIsAddedWithVersion2" );
+			}
+		}
+	}
+	 */
+	void OnModStoreSave( ModStorage storage, string modName )
+	{
+
+	}
+
+	/**
+	 * @param storage	Where the data is read from, only use public methods 'bool Read...(out value)`
+	 * @param modName	The name of the mod from CfgMods class to check against
+	 * 
+	 * @code
+	modded class Sedan_02 // extends from AnimalBase
+	{
+		override bool OnModStoreLoad( ModStorage storage, string modName )
+		{
+			if ( !super.OnModStoreLoad( storage, modName ) )
+				return false;
+
+			if ( modName != "JM_CommunityFramework" )
+				return true; //! Early exit
+
+			vector orientation;
+			if ( !storage.ReadVector( orientation ) )
+				return false;
+
+			string intVar;
+			if ( !storage.ReadInt( intVar ) )
+				return false;
+
+			//! The version of the mod is set in 'ModStructure::OnLoad', using 'SetStorageVersion'
+			if ( storage.GetVersion() > 1 )
+			{
+				string strVar;
+				if ( !storage.ReadString( strVar ) )
+					return false;
+			}
+
+			return true;
+		}
+	}
+	 */
+	bool OnModStoreLoad( ModStorage storage, string modName )
+	{
+		return true;
+	}
+};
--- JM/CF/Scripts/3_Game/CommunityFramework/Mods/ModLoader.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/Mods/ModLoader.c
@@ -1,37 +1,37 @@
 modded class ModLoader
 {
-	private static ref map< string, ModStructure > m_ModMap = new map< string, ModStructure >();
+	private static ref map<string, ModStructure> m_ModMap = new map<string, ModStructure>();
 
-	static ref ModStructure Get( string name )
+	static ref ModStructure Get(string name)
 	{
-		return m_ModMap[ name ];
+		return m_ModMap[name];
 	}
 
-	override static array< ref ModStructure > GetMods()
+	override static array<ref ModStructure> GetMods()
 	{
-		if( !m_Loaded )
+		if (!m_Loaded)
 			LoadMods();
 		return m_Mods;
 	}
-	
+
 	override static void LoadMods()
 	{
-		m_Mods = new array< ref ModStructure >;
-		
-		int mod_count = GetGame().ConfigGetChildrenCount( "CfgMods" );
-		
-		for( int i = 2; i < mod_count; i++ )
+		m_Mods = new array<ref ModStructure>;
+
+		int mod_count = GetGame().ConfigGetChildrenCount("CfgMods");
+
+		for (int i = 2; i < mod_count; i++)
 		{
 			string mod_name;
-			GetGame().ConfigGetChildName( "CfgMods", i, mod_name );
+			GetGame().ConfigGetChildName("CfgMods", i, mod_name);
 
-			ref ModStructure mod = new ModStructure( i, "CfgMods " + mod_name );
-			
-			m_Mods.Insert( mod );
+			ref ModStructure mod = new ModStructure(i, "CfgMods " + mod_name);
 
-			m_ModMap.Insert( mod_name, mod );
+			m_Mods.Insert(mod);
+
+			m_ModMap.Insert(mod_name, mod);
 		}
-		
+
 		m_Loaded = true;
 	}
 };
