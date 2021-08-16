ea9324010edfd7b0264b239b04f358d9d559bae4
Jacob <mangosprograms@gmail.com>
2021-08-16 17:59:03 +1000

    Enable all events for 'JMModuleBase'

--- JM/CF/Scripts/4_World/CommunityFramework/Module/JMModuleBase.c
+++ JM/CF/Scripts/4_World/CommunityFramework/Module/JMModuleBase.c
@@ -1,8 +1,35 @@
 class JMModuleBase : CF_WorldModule
 {
+	/**
+	 * Any module that inherits from 'JMModuleBase' probably expects all events to be enabled
+	 */
 	override void OnInit()
 	{
 		super.OnInit();
+
+		EnableUpdate();
+		EnableMissionStart();
+		EnableMissionFinish();
+		EnableMissionLoaded();
+		EnableRPC();
+		EnableWorldCleanup();
+		EnableMPSessionStart();
+		EnableMPSessionPlayerReady();
+		EnableMPSessionFail();
+		EnableMPSessionEnd();
+		EnableMPConnectAbort();
+		EnableMPConnectionLost();
+		EnableRespawn();
+		EnableInvokeConnect();
+		EnableInvokeDisconnect();
+		EnableClientNew();
+		EnableClientRespawn();
+		EnableClientReady();
+		EnableClientPrepare();
+		EnableClientLogoutReconnect();
+		EnableClientLogout();
+		EnableClientDisconnect();
+		EnableClientLogoutCancelled();
 	}
 
 	void RegisterKeyMouseBinding(JMModuleBinding binding) 
