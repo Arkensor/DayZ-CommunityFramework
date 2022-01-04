a71a1658794cb5a3f0d3e2475355272f62a7e9b3
Jacob <mangosprograms@gmail.com>
2022-01-05 03:23:36 +1100

    added path, added copy to file, removed setfolder

--- JM/CF/Scripts/1_Core/CommunityFramework/Files/CF_File.c
+++ JM/CF/Scripts/1_Core/CommunityFramework/Files/CF_File.c
@@ -1,10 +1,9 @@
 class CF_File : Managed
 {
-	static string DIRECTORY_SEPARATOR = "/";
-	static string FILESYSTEM_IDENTIFIER = ":";
-
+	protected string m_Directory;
 	protected string m_FileName;
-	protected string m_Folder;
+	protected string m_Extension;
+
 	protected bool m_IsDirectory;
 	protected bool m_IsHidden;
 	protected bool m_IsReadOnly;
@@ -28,7 +27,7 @@ class CF_File : Managed
 			files = new array<ref CF_File>();
 		}
 
-		pattern.Replace("\\", CF_File.DIRECTORY_SEPARATOR);
+		pattern.Replace("\\", CF_Path.DIRECTORY_SEPARATOR);
 
 		string fileName;
 		FileAttr fileAttr;
@@ -38,22 +37,21 @@ class CF_File : Managed
 			return false;
 		}
 
-		string folder = PatternToFolder(pattern);
+		string folder = CF_Path.GetDirectoryName(pattern);
 
 		CF_File file;
 		while (true)
 		{
 			file = new CF_File();
-			file.m_FileName = fileName;
 
-			if (file.SetFolder(folder))
-			{
-				file.m_IsDirectory = fileAttr & FileAttr.DIRECTORY;
-				file.m_IsHidden = fileAttr & FileAttr.HIDDEN;
-				file.m_IsReadOnly = fileAttr & FileAttr.READONLY;
-				file.m_IsValid = !(fileAttr & FileAttr.INVALID);
-				files.Insert(file);
-			}
+			file.SetFileName(fileName);
+			file.SetFolder(folder);
+
+			file.m_IsDirectory = (fileAttr & FileAttr.DIRECTORY) != 0;
+			file.m_IsHidden = (fileAttr & FileAttr.HIDDEN) != 0;
+			file.m_IsReadOnly = (fileAttr & FileAttr.READONLY) != 0;
+			file.m_IsValid = (fileAttr & FileAttr.INVALID) != 0;
+			files.Insert(file);
 
 			if (!FindNextFile(handle, fileName, fileAttr))
 			{
@@ -65,24 +63,36 @@ class CF_File : Managed
 		return true;
 	}
 
-	static string PatternToFolder(string pattern)
+	/**
+	 * @brief Constructor
+	 * 
+	 * @param path No operation perform if path is empty string
+	 */
+	void CF_File(string path = "")
 	{
-		int index;
-		CF_String folder = pattern;
+		m_IsValid = true;
 
-		index = folder.CF_LastIndexOf(CF_File.DIRECTORY_SEPARATOR);
-		if (index != -1)
+		// Default constructor
+		if (path == string.Empty)
 		{
-			return folder.Substring(0, index) + CF_File.DIRECTORY_SEPARATOR;
+			return;
 		}
 
-		index = folder.CF_LastIndexOf(CF_File.FILESYSTEM_IDENTIFIER);
-		if (index != -1)
+		string fileName;
+		FileAttr fileAttr;
+		FindFileHandle handle = FindFile(path, fileName, fileAttr, FindFileFlags.ALL);
+		if (!handle)
 		{
-			return folder.Substring(0, index) + CF_File.FILESYSTEM_IDENTIFIER;
+			return;
 		}
+		
+		SetFolder(CF_Path.GetDirectoryName(path));
+		SetFileName(fileName);
 
-		return "";
+		m_IsDirectory = (fileAttr & FileAttr.DIRECTORY) != 0;
+		m_IsHidden = (fileAttr & FileAttr.HIDDEN) != 0;
+		m_IsReadOnly = (fileAttr & FileAttr.READONLY) != 0;
+		m_IsValid = (fileAttr & FileAttr.INVALID) != 0;
 	}
 
 	bool IsDirectory()
@@ -102,51 +112,51 @@ class CF_File : Managed
 
 	bool IsValid()
 	{
-		if (m_Folder == string.Empty)
-		{
-			return false;
-		}
-
 		return m_IsValid;
 	}
 
+	/**
+	 * @brief Returns the path, including file name and extension if they exist
+	 */
+	string GetFullPath()
+	{
+		return m_Directory + m_FileName + m_Extension;
+	}
+
+	/**
+	 * @brief Returns the file name and extension
+	 */
 	string GetFileName()
+	{
+		return m_FileName + m_Extension;
+	}
+
+	/**
+	 * @brief Returns the file name without the extension 
+	 */
+	string GetFileNameWithoutExtension()
 	{
 		return m_FileName;
 	}
 
 	/**
-	 * @brief Sets the folder the file is in
-	 *
-	 * @param folder The folder the file is in
-	 * 
-	 * @return true if the file does exist in the designated folder
+	 * @brief Returns the extension (including the period ".")
 	 */
-	protected bool SetFolder(string folder)
+	string GetExtension()
 	{
-#ifdef CF_TRACE_ENABLED
-		auto trace = CF_Trace_1(this, "SetFolder").Add(folder);
-#endif
-
-		if (!FileExist(folder + m_FileName))
-		{
-			CF_Log.Error("Failed to set folder for file \"%1\" to \"%2\"", m_FileName, folder);
-			return false;
-		}
-
-		m_Folder = folder;
-		return true;
+		return m_Extension;
 	}
 
-	string GetFolder()
+	/**
+	 * @brief Returns the full path of the parent folder
+	 */
+	string GetDirectory()
 	{
-		return m_Folder;
+		return m_Directory;
 	}
 
 	/**
 	 * @brief Creates and opens a new instance of a FileStream for the current file
-	 * 
-	 * @note 'SetFolder' must be called on this instance beforehand
 	 * 
 	 * @param mode Sets the file mode the stream will be opened in
 	 * 
@@ -163,7 +173,7 @@ class CF_File : Managed
 			return null;
 		}
 
-		return new CF_FileStream(m_Folder + m_FileName, mode);
+		return new CF_FileStream(GetFullPath(), mode);
 	}
 
 	/**
@@ -182,7 +192,7 @@ class CF_File : Managed
 			return false;
 		}
 
-		return DeleteFile(m_Folder + m_FileName);
+		return DeleteFile(GetFullPath());
 	}
 
 	/**
@@ -204,41 +214,40 @@ class CF_File : Managed
 		}
 
 		// Check if the file already exists at the destination
-		if (FileExist(m_Folder + name))
+		if (FileExist(m_Directory + name))
 		{
 			return false;
 		}
 
 		// Copy file to new name
-		if (!CopyFile(m_Folder + m_FileName, m_Folder + name))
+		if (!CopyFile(GetFullPath(), m_Directory + name))
 		{
 			return false;
 		}
 
 		// Delete old file
-		bool success = DeleteFile(m_Folder + m_FileName);
+		bool success = DeleteFile(GetFullPath());
 		if (!success)
 		{
-			CF_Log.Warn("Failed to delete original file in rename for file \"%1%2\"", m_Folder, m_FileName);
+			CF_Log.Warn("Failed to delete original file in rename for file \"%1\"", GetFullPath());
 		}
 
-		m_FileName = name;
+		SetFileName(name);
 
 		return true;
 	}
 
 	/**
-	 * @brief Moves the file to the new folder (copy to folder, then delete original)
+	 * @brief Moves the file to the new path (copy to folder, then delete original)
 	 * 
-	 * @param folder The new parent folder
-	 * @param name The new name of the file
+	 * @param path The new path
 	 * 
 	 * @return true if the operation was successful
 	 */
-	bool Move(string folder, string name = "")
+	bool Move(string path)
 	{
 #ifdef CF_TRACE_ENABLED
-		auto trace = CF_Trace_2(this, "Move").Add(folder).Add(name);
+		auto trace = CF_Trace_1(this, "Move").Add(path);
 #endif
 
 		if (!IsValid())
@@ -246,10 +255,13 @@ class CF_File : Managed
 			return false;
 		}
 
+		string folder = CF_Path.GetDirectoryName(path);
+		string name = CF_Path.GetFileNameEx(path, folder);
+
 		// File isn't being renamed in move
 		if (name == string.Empty)
 		{
-			name = m_FileName;
+			name = GetFileName();
 		}
 
 		// Check if the file already exists at the destination
@@ -258,26 +270,134 @@ class CF_File : Managed
 			return false;
 		}
 
-		if (!CopyFile(m_Folder + m_FileName, folder + name))
+		if (!CopyFile(GetFullPath(), folder + name))
 		{
 			return false;
 		}
 
 		// Delete old file
-		bool success = DeleteFile(m_Folder + m_FileName);
+		bool success = DeleteFile(GetFullPath());
 		if (!success)
 		{
-			CF_Log.Warn("Failed to delete original file in move for file \"%1%2\"", m_Folder, m_FileName);
+			CF_Log.Warn("Failed to delete original file in move for file \"%1\"", GetFullPath());
 		}
 
-		m_FileName = name;
-		m_Folder = folder;
+		SetFolder(folder);
+		SetFileName(name);
+
+		return true;
+	}
+
+	/**
+	 * @brief Copies the file to the new path
+	 * 
+	 * @param path The new path
+	 * 
+	 * @return true if the operation was successful
+	 */
+	bool Copy(string path)
+	{
+#ifdef CF_TRACE_ENABLED
+		auto trace = CF_Trace_1(this, "Move").Add(path);
+#endif
+
+		if (!IsValid())
+		{
+			return false;
+		}
+
+		string folder = CF_Path.GetDirectoryName(path);
+		string name = CF_Path.GetFileNameEx(path, folder);
+
+		// File isn't being renamed in move
+		if (name == string.Empty)
+		{
+			name = GetFileName();
+		}
+
+		// Check if the file already exists at the destination
+		if (FileExist(folder + name))
+		{
+			CF_Log.Warn("The file already exists at \"%1\".", folder + name);
+			return false;
+		}
+
+		if (!CopyFile(GetFullPath(), folder + name))
+		{
+			CF_Log.Warn("Failed to copy file to \"%1\".", folder + name);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @brief Copies the file to the new path, returning the instance of the file
+	 * 
+	 * @param path The new path
+	 * @param [out] newFile Outputs the new instance of the file
+	 * 
+	 * @return true if the operation was successful
+	 */
+	bool Copy(string path, out CF_File newFile)
+	{
+#ifdef CF_TRACE_ENABLED
+		auto trace = CF_Trace_1(this, "Copy").Add(path);
+#endif
+
+		if (!IsValid())
+		{
+			return false;
+		}
+
+		string folder = CF_Path.GetDirectoryName(path);
+		string name = CF_Path.GetFileNameEx(path, folder);
+
+		// File isn't being renamed in move
+		if (name == string.Empty)
+		{
+			name = GetFileName();
+		}
+
+		// Check if the file already exists at the destination
+		if (FileExist(folder + name))
+		{
+			CF_Log.Warn("The file already exists at \"%1\".", folder + name);
+			return false;
+		}
+
+		if (!CopyFile(GetFullPath(), folder + name))
+		{
+			CF_Log.Warn("Failed to copy file to \"%1\".", folder + name);
+			return false;
+		}
+
+		newFile = new CF_File(path);
 
 		return true;
 	}
 
 	override string GetDebugName()
 	{
-		return "{path=\"" + m_Folder + m_FileName + "\", filename=\"" + m_FileName + "\", directory=" + m_IsDirectory.ToString() + ", hidden=" + m_IsHidden.ToString() + ", readonly=" + m_IsReadOnly.ToString() + ", valid=" + m_IsValid.ToString() + "}";
+		return "{path=\"" + GetFullPath() + "\", filename=\"" + GetFileName() + "\", directory=" + m_IsDirectory.ToString() + ", hidden=" + m_IsHidden.ToString() + ", readonly=" + m_IsReadOnly.ToString() + ", valid=" + m_IsValid.ToString() + "}";
+	}
+
+	protected void SetFolder(string folder)
+	{
+		m_Directory = folder;
+	}
+
+	protected void SetFileName(CF_String fileName)
+	{
+		int index = fileName.CF_LastIndexOf(".");
+		if (index == -1)
+		{
+			m_FileName = fileName;
+			m_Extension = "";
+			return;
+		}
+
+		m_Extension = fileName.Substring(index, fileName.Length() - index);
+		m_FileName = fileName.Substring(0, fileName.Length() - m_Extension.Length());
 	}
 };
---
+++ docs/File/index.md
@@ -0,0 +1,14 @@
+# File
+
+Using `CF_File` you can get and iterate over all the script.log files within the dayz profile directory.
+
+```csharp
+array<ref CF_File> files();
+CF_File.Find("$profile:script*", files);
+foreach (auto file : files)
+{
+	Print(file.GetDebugName());
+}
+```
+
+There are also helper file functions that allow you to `Delete`, `Rename`, `Copy` and `Move`.
