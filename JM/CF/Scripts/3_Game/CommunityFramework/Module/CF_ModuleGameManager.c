9df14456104d79c3d793ac6ec37536a5532a6c6c
Jacob <mangosprograms@gmail.com>
2022-01-23 15:20:21 +1100

    Compile error, documented 'CF_ModuleGameManager::UpdateGameFlag

--- JM/CF/Scripts/3_Game/CommunityFramework/Module/CF_ModuleGameManager.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/Module/CF_ModuleGameManager.c
@@ -3,13 +3,21 @@ class CF_ModuleGameManager
 	static ref CF_ModuleGameEvent s_RPC = new CF_ModuleGameEvent();
 	static ref CF_ModuleGameEvent s_UpdateInput = new CF_ModuleGameEvent();
 
+	/**
+	 * @brief Updates the flag for event calling. So server side modules don't get the event called on client and same for client side on server.
+	 * 
+	 * @param game Passes in the CGame/DayZGame instance. If none provided, automatically obtains from global 'g_Game' variable
+	 */
 	static void UpdateGameFlag(CGame game = null)
 	{
 		if (game == null)
 		{
-			game = GetGame();
+			game = g_Game;
 		}
 
+		// 0xFF = Single Player
+		// 0x0F = Client
+		// 0xF0 = Dedicated Server
 		CF_ModuleCoreManager.s_GameFlag = 0xFF;
 		if (game.IsMultiplayer())
 		{
--- JM/CF/Scripts/5_Mission/CommunityFramework/Mission/MissionBase.c
+++ JM/CF/Scripts/5_Mission/CommunityFramework/Mission/MissionBase.c
@@ -4,17 +4,10 @@ modded class MissionBase
 	{
 		CF_ModuleGameManager.UpdateGameFlag();
 
-		CreateDiscordManager();
-		CreateSteamManager();
-
-		if ( !ModuleManagerExists() )
+		// Deprecated
+		if (!ModuleManagerExists())
 		{
-			Print( "Creating ModuleManager" );
-
-			CreateModuleManager( new JMModuleConstructor );
-		} else
-		{
-			Print( "Ignoring creation of ModuleManager" );
+			CreateModuleManager(new JMModuleConstructor());
 		}
 	}
 };
