419eec42eeb69b410ee96613eac284e4204c0156
Jacob <mangosprograms@gmail.com>
2022-01-23 15:14:28 +1100

    RPCFramework: Removed disabled stat tracking, reverted some ref changes

--- JM/CF/Scripts/3_Game/CommunityFramework/RPC/RPCManager.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/RPC/RPCManager.c
@@ -44,182 +44,36 @@ class RPCMetaWrapper
 
 class RPCManager
 {
-	protected float m_Timer;
+	static const int FRAMEWORK_RPC_ID = 10042;
 
-	protected int m_AmountSent;
-	protected int m_AmountSentInUpdate;
-	protected int m_AmountSentInSecond;
-
-	protected int m_AmountRecieved;
-	protected int m_AmountRecievedInUpdate;
-	protected int m_AmountRecievedInSecond;
-
-	protected int m_AmountSuccessRecieved;
-	protected int m_AmountSuccessRecievedInUpdate;
-	protected int m_AmountSuccessRecievedInSecond;
-
-	protected int m_AmountPreviousSent;
-	protected int m_AmountPreviousSentInUpdate;
-	protected int m_AmountPreviousSentInSecond;
-
-	protected int m_AmountPreviousRecieved;
-	protected int m_AmountPreviousRecievedInUpdate;
-	protected int m_AmountPreviousRecievedInSecond;
-
-	protected int m_AmountPreviousSuccessRecieved;
-	protected int m_AmountPreviousSuccessRecievedInUpdate;
-	protected int m_AmountPreviousSuccessRecievedInSecond;
-
-	protected string m_UpdateChecker;
-
-	protected const int FRAMEWORK_RPC_ID = 10042;
-	protected autoptr map< string, ref map< string, ref RPCMetaWrapper > > m_RPCActions;
-
-	protected bool m_RPCManagerEnabled = false;
-
-	void RPCManager()
-	{
-		#ifdef CF_TRACE_ENABLED
-		auto trace = CF_Trace_0(this, "RPCManager");
-		#endif
-
-		m_RPCActions = new map< string, ref map< string, ref RPCMetaWrapper > >;
-
-		m_UpdateChecker = "JM_CF_RPC";
-
-		if ( GetGame().IsClient() )
-			m_UpdateChecker = "JM_CF_RPC_Client";
-	}
-
-	void ~RPCManager()
-	{
-		#ifdef CF_TRACE_ENABLED
-		auto trace = CF_Trace_0(this, "~RPCManager");
-		#endif
-
-		GetDayZGame().Event_OnRPC.Remove( OnRPC );
-		delete m_RPCActions;
-	}
+	protected autoptr map< string, ref map< string, ref RPCMetaWrapper > > m_RPCActions = new map< string, ref map< string, ref RPCMetaWrapper > >;
 	
-	void OnUpdate( float timeSlice )
-	{
-		#ifdef CF_TRACE_ENABLED
-		auto trace = CF_Trace_1(this, "OnUpdate").Add(timeSlice);
-		#endif
-
-		string str = "";
-		bool anyChanged = false;
-
-		if ( m_AmountPreviousSent != m_AmountSent )
-		{
-			anyChanged = true;
-			str = str + "S T: " + m_AmountSent + " | ";
-		}
-		if ( m_AmountPreviousSentInUpdate != m_AmountSentInUpdate )
-		{
-			anyChanged = true;
-			str = str + "S 1s: " + m_AmountSentInSecond + " | ";
-		}
-		if ( m_AmountPreviousSentInSecond != m_AmountSentInSecond )
-		{
-			anyChanged = true;
-			str = str + "S " + timeSlice + "s: " + m_AmountSentInUpdate + " | ";
-		}
-
-		if ( m_AmountPreviousRecieved != m_AmountRecieved )
-		{
-			anyChanged = true;
-			str = str + "R T: " + m_AmountRecieved + " | ";
-		}
-		if ( m_AmountPreviousRecievedInUpdate != m_AmountRecievedInUpdate )
-		{
-			anyChanged = true;
-			str = str + "R 1s: " + m_AmountRecievedInSecond + " | ";
-		}
-		if ( m_AmountPreviousRecievedInSecond != m_AmountRecievedInSecond )
-		{
-			anyChanged = true;
-			str = str + "R " + timeSlice + "s: " + m_AmountRecievedInUpdate + " | ";
-		}
-
-		if ( m_AmountPreviousSuccessRecieved != m_AmountSuccessRecieved )
-		{
-			anyChanged = true;
-			str = str + "S R T: " + m_AmountSuccessRecieved + " | ";
-		}
-		if ( m_AmountPreviousSuccessRecievedInUpdate != m_AmountSuccessRecievedInUpdate )
-		{
-			anyChanged = true;
-			str = str + "S R 1s: " + m_AmountSuccessRecievedInSecond + " | ";
-		}
-		if ( m_AmountPreviousSuccessRecievedInSecond != m_AmountSuccessRecievedInSecond )
-		{
-			anyChanged = true;
-			str = str + "S R " + timeSlice + "s: " + m_AmountSuccessRecievedInUpdate;
-		}
-
-		m_AmountSentInUpdate = 0;
-		m_AmountRecievedInUpdate = 0;
-		m_AmountSuccessRecievedInUpdate = 0;
-
-		m_Timer += timeSlice;
-
-		if ( m_Timer >= 1.0 )
-		{
-			m_Timer = 0;
-
-			m_AmountSentInSecond = 0;
-			m_AmountRecievedInSecond = 0;
-			m_AmountSuccessRecievedInSecond = 0;
-		}
-
-		m_AmountPreviousSent = m_AmountSent;
-		m_AmountPreviousSentInUpdate = m_AmountSentInUpdate;
-		m_AmountPreviousSentInSecond = m_AmountSentInSecond;
-		m_AmountPreviousRecieved = m_AmountRecieved;
-		m_AmountPreviousRecievedInUpdate = m_AmountRecievedInUpdate;
-		m_AmountPreviousRecievedInSecond = m_AmountRecievedInSecond;
-		m_AmountPreviousSuccessRecieved = m_AmountSuccessRecieved;
-		m_AmountPreviousSuccessRecievedInUpdate = m_AmountSuccessRecievedInUpdate;
-		m_AmountPreviousSuccessRecievedInSecond = m_AmountSuccessRecievedInSecond;
-	}
-
 	void OnRPC( PlayerIdentity sender, Object target, int rpc_type, ParamsReadContext ctx )
 	{
 		#ifdef CF_TRACE_ENABLED
 		auto trace = CF_Trace_4(this, "OnRPC").Add(sender).Add(target).Add(rpc_type).Add(ctx);
 		#endif
-
-		if ( rpc_type != FRAMEWORK_RPC_ID )
-		{
-			return;
-		}
 		
 		Param2< string, string > metaData;
 		if ( !ctx.Read( metaData ) )
 		{
-			//Error( "Failed reading the RPC metadata!");
+			CF_Log.Warn( "Failed reading the RPC metadata!");
 			return;
 		}
-
-		m_AmountRecieved++;
-		m_AmountRecievedInUpdate++;
-		m_AmountRecievedInSecond++;
 		
 		string modName = metaData.param1;
 		string funcName = metaData.param2;
 
 		string recievedFrom = "server";
-
-		if ( GetGame().IsServer() && GetGame().IsMultiplayer() )
+		if ( GetGame().IsDedicatedServer() )
 		{
 			if ( sender == NULL )
 			{
-				recievedFrom = "someone";
-			} else 
-			{
-				recievedFrom = sender.GetPlainId();
+				CF_Log.Warn( "RPC sent from invalid sender!");
+				return;
 			}
+
+			recievedFrom = sender.GetPlainId();
 		}
 		
 		map< string, ref RPCMetaWrapper > functions;
@@ -230,10 +84,6 @@ class RPCManager
 			{
 				if ( wrapper.GetInstance() )
 				{
-					m_AmountSuccessRecieved++;
-					m_AmountSuccessRecievedInUpdate++;
-					m_AmountSuccessRecievedInSecond++;
-
 					auto functionCallData = new Param4< CallType, ref ParamsReadContext, ref PlayerIdentity, ref Object >( CallType.Server, ctx, sender, target );
 				
 					if ( ( GetGame().IsServer() && GetGame().IsMultiplayer() ) || ( GetGame().IsServer() && !GetGame().IsMultiplayer() && ( wrapper.GetSPExecutionType() == SingleplayerExecutionType.Server || wrapper.GetSPExecutionType() == SingleplayerExecutionType.Both ) ) ) 
@@ -250,39 +100,22 @@ class RPCManager
 				}
 			} else
 			{
-				Error( recievedFrom + " tried sending " + modName + "::<" + funcName + "> which does not seem to exist!");
+				CF_Log.Warn( recievedFrom + " tried sending " + modName + "::<" + funcName + "> which does not seem to exist!");
 			}
 		} else
 		{
-			Error( recievedFrom + " tried sending <" + modName + ">::" + funcName + " which does not seem to exist!");
+			CF_Log.Warn( recievedFrom + " tried sending <" + modName + ">::" + funcName + " which does not seem to exist!");
 		}
 	}
 
-	void SendRPC( string modName, string funcName, ref Param params = NULL, bool guaranteed = false, PlayerIdentity sendToIdentity = NULL, Object sendToTarget = NULL )
+	void SendRPC( string modName, string funcName, ref Param params = NULL, bool guaranteed = false, ref PlayerIdentity sendToIdentity = NULL, ref Object sendToTarget = NULL )
 	{
 		#ifdef CF_TRACE_ENABLED
 		auto trace = CF_Trace_6(this, "SendRPC").Add(modName).Add(funcName).Add(params).Add(guaranteed).Add(sendToIdentity).Add(sendToTarget);
 		#endif
 
-		m_AmountSent++;
-		m_AmountSentInUpdate++;
-		m_AmountSentInSecond++;
-		
-		string sendTo = "server";
-
-		if ( GetGame().IsServer() && GetGame().IsMultiplayer() )
-		{
-			if ( sendToIdentity == NULL )
-			{
-				sendTo = "everyone";
-			} else 
-			{
-				sendTo = sendToIdentity.GetPlainId();
-			}
-		}
-
 		auto sendData = new array< ref Param >;
-		sendData.Insert( new ref Param2< string, string >( modName, funcName ) );
+		sendData.Insert( new Param2< string, string >( modName, funcName ) );
 		sendData.Insert( params );
 		
 		//In case we are in the singleplayer and the data is consumed twice for both client and server, we need to add it twice. Better than making a deep copy with more complicated rules on receiving
@@ -308,28 +141,11 @@ class RPCManager
 	/**
 	 * Warning: Does not support "SingleplayerExecutionType.Both"
 	 */
-	void SendRPCs( string modName, string funcName, array< ref Param > params, bool guaranteed = false, PlayerIdentity sendToIdentity = NULL, Object sendToTarget = NULL )
+	void SendRPCs( string modName, string funcName, ref array< ref Param > params, bool guaranteed = false, ref PlayerIdentity sendToIdentity = NULL, ref Object sendToTarget = NULL )
 	{
 		#ifdef CF_TRACE_ENABLED
 		auto trace = CF_Trace_6(this, "SendRPCs").Add(modName).Add(funcName).Add(params).Add(guaranteed).Add(sendToIdentity).Add(sendToTarget);
 		#endif
-
-		m_AmountSent++;
-		m_AmountSentInUpdate++;
-		m_AmountSentInSecond++;
-
-		string sendTo = "server";
-
-		if ( GetGame().IsServer() && GetGame().IsMultiplayer() )
-		{
-			if ( sendToIdentity == NULL )
-			{
-				sendTo = "everyone";
-			} else 
-			{
-				sendTo = sendToIdentity.GetPlainId();
-			}
-		}
 
 		params.InsertAt( new Param2< string, string >( modName, funcName ), 0 );
 
@@ -345,7 +161,7 @@ class RPCManager
 					
 					if ( ( wrapper.GetSPExecutionType() == SingleplayerExecutionType.Both ) )
 					{
-						Error( modName + "::" + funcName + " does not support \"SingleplayerExecutionType.Both\" when using RPCManager::SendRPCs, use RPCManager::SendRPC instead!");
+						CF_Log.Warn( modName + "::" + funcName + " does not support \"SingleplayerExecutionType.Both\" when using RPCManager::SendRPCs, use RPCManager::SendRPC instead!");
 					}
 				}
 			}
@@ -366,13 +182,6 @@ class RPCManager
 		auto wrapper = new RPCMetaWrapper( instance, singlePlayerExecType );
 		
 		m_RPCActions[ modName ].Set( funcName, wrapper );
-
-		if ( !m_RPCManagerEnabled )
-		{
-			GetDayZGame().Event_OnRPC.Insert( OnRPC );
-
-			m_RPCManagerEnabled = true;
-		}
 
 		return true;
 	}
