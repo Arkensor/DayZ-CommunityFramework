73ccfb449fbfaf2dc2d28637507021f976f052b3
Jacob <mangosprograms@gmail.com>
2022-01-28 20:03:23 +1100

    Fixes a 1.16 compile error

--- JM/CF/Scripts/4_World/CommunityFramework/Surfaces/CF_Surface.c
+++ JM/CF/Scripts/4_World/CommunityFramework/Surfaces/CF_Surface.c
@@ -49,7 +49,7 @@ class CF_Surface
 	/**
 	 * @brief Private constructor to prevent from marking as ref
 	 */
-	private void ~CF_Surface()
+	/*private*/ void ~CF_Surface()
 	{
 #ifdef CF_TRACE_ENABLED
 		auto trace = CF_Trace_0(this, "~CF_Surface");
