bb116e3005362bcdc9e1d8607e71370a8e06c15b
Jacob <mangosprograms@gmail.com>
2022-02-08 17:09:32 +1100

    Fixed a crash possibility

--- JM/CF/Scripts/1_Core/CommunityFramework/Logging/CF_Trace.c
+++ JM/CF/Scripts/1_Core/CommunityFramework/Logging/CF_Trace.c
@@ -28,11 +28,11 @@ class CF_Trace : Managed
 		}
 		
 		string instanceName;
-		g_Script.CallFunction(instance, "GetDebugNameNative", instanceName, null);
-		if (instanceName == string.Empty)
-		{
+		//g_Script.CallFunction(instance, "GetDebugNameNative", instanceName, null);
+		//if (instanceName == string.Empty)
+		//{
 			instanceName = "" + instance;
-		}
+		//}
 
 		return instanceName;
 	}
@@ -45,7 +45,7 @@ class CF_Trace : Managed
 		return res;
 	}
 
-	void CF_Trace(string instanceName, string stackName, int count, bool doLog = true)
+	void CF_Trace(string instanceName, string stackName, int count, bool doLog)
 	{
 		m_DoLog = doLog;
 
@@ -202,157 +202,157 @@ class CF_Trace : Managed
 
 static CF_Trace CF_Trace_Instance(Class instance)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), string.Empty, 0);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), string.Empty, 0, true);
 }
 
 static CF_Trace CF_Trace_0(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 0);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 0, true);
 }
 
 static CF_Trace CF_Trace_1(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 1);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 1, true);
 }
 
 static CF_Trace CF_Trace_2(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 2);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 2, true);
 }
 
 static CF_Trace CF_Trace_3(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 3);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 3, true);
 }
 
 static CF_Trace CF_Trace_4(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 4);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 4, true);
 }
 
 static CF_Trace CF_Trace_5(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 5);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 5, true);
 }
 
 static CF_Trace CF_Trace_6(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 6);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 6, true);
 }
 
 static CF_Trace CF_Trace_7(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 7);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 7, true);
 }
 
 static CF_Trace CF_Trace_8(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 8);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 8, true);
 }
 
 static CF_Trace CF_Trace_9(Class instance, string stackName)
 {
-	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 9);
+	return new CF_Trace(CF_Trace.FormatInstance(instance), stackName, 9, true);
 }
 
 static CF_Trace CF_Trace_0(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 0);
+	return new CF_Trace(instance, stackName, 0, true);
 }
 
 static CF_Trace CF_Trace_1(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 1);
+	return new CF_Trace(instance, stackName, 1, true);
 }
 
 static CF_Trace CF_Trace_2(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 2);
+	return new CF_Trace(instance, stackName, 2, true);
 }
 
 static CF_Trace CF_Trace_3(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 3);
+	return new CF_Trace(instance, stackName, 3, true);
 }
 
 static CF_Trace CF_Trace_4(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 4);
+	return new CF_Trace(instance, stackName, 4, true);
 }
 
 static CF_Trace CF_Trace_5(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 5);
+	return new CF_Trace(instance, stackName, 5, true);
 }
 
 static CF_Trace CF_Trace_6(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 6);
+	return new CF_Trace(instance, stackName, 6, true);
 }
 
 static CF_Trace CF_Trace_7(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 7);
+	return new CF_Trace(instance, stackName, 7, true);
 }
 
 static CF_Trace CF_Trace_8(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 8);
+	return new CF_Trace(instance, stackName, 8, true);
 }
 
 static CF_Trace CF_Trace_9(string instance, string stackName)
 {
-	return new CF_Trace(instance, stackName, 9);
+	return new CF_Trace(instance, stackName, 9, true);
 }
 
 static CF_Trace CF_Trace_0(string stackName)
 {
-	return new CF_Trace("", stackName, 0);
+	return new CF_Trace("", stackName, 0, true);
 }
 
 static CF_Trace CF_Trace_1(string stackName)
 {
-	return new CF_Trace("", stackName, 1);
+	return new CF_Trace("", stackName, 1, true);
 }
 
 static CF_Trace CF_Trace_2(string stackName)
 {
-	return new CF_Trace("", stackName, 2);
+	return new CF_Trace("", stackName, 2, true);
 }
 
 static CF_Trace CF_Trace_3(string stackName)
 {
-	return new CF_Trace("", stackName, 3);
+	return new CF_Trace("", stackName, 3, true);
 }
 
 static CF_Trace CF_Trace_4(string stackName)
 {
-	return new CF_Trace("", stackName, 4);
+	return new CF_Trace("", stackName, 4, true);
 }
 
 static CF_Trace CF_Trace_5(string stackName)
 {
-	return new CF_Trace("", stackName, 5);
+	return new CF_Trace("", stackName, 5, true);
 }
 
 static CF_Trace CF_Trace_6(string stackName)
 {
-	return new CF_Trace("", stackName, 6);
+	return new CF_Trace("", stackName, 6, true);
 }
 
 static CF_Trace CF_Trace_7(string stackName)
 {
-	return new CF_Trace("", stackName, 7);
+	return new CF_Trace("", stackName, 7, true);
 }
 
 static CF_Trace CF_Trace_8(string stackName)
 {
-	return new CF_Trace("", stackName, 8);
+	return new CF_Trace("", stackName, 8, true);
 }
 
 static CF_Trace CF_Trace_9(string stackName)
 {
-	return new CF_Trace("", stackName, 9);
+	return new CF_Trace("", stackName, 9, true);
 }
 
 static CF_Trace CF_Trace_Instance(bool doLog, Class instance)
