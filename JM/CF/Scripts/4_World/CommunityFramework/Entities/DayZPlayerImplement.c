613d5ae1befd80b0b140a8bae30794a87a801b9f
Jacob <mangosprograms@gmail.com>
2022-02-15 23:40:31 +1100

    Proper disable

--- JM/CF/Scripts/4_World/CommunityFramework/Entities/DayZPlayerImplement.c
+++ JM/CF/Scripts/4_World/CommunityFramework/Entities/DayZPlayerImplement.c
@@ -1,23 +1,23 @@
 modded class DayZPlayerImplement
 {
-	autoptr CF_ModStorageBase m_CF_ModStorage = new CF_ModStorageObject<DayZPlayerImplement>(this);
-
-	override void OnStoreSave(ParamsWriteContext ctx)
-	{
-		super.OnStoreSave(ctx);
-
-		m_CF_ModStorage.OnStoreSave(ctx);
-	}
-
-	override bool OnStoreLoad(ParamsReadContext ctx, int version)
-	{
-		if (!super.OnStoreLoad(ctx, version))
-		{
-			return false;
-		}
-
-		return m_CF_ModStorage.OnStoreLoad(ctx, version);
-	}
+//	autoptr CF_ModStorageBase m_CF_ModStorage = new CF_ModStorageObject<DayZPlayerImplement>(this);
+//
+//	override void OnStoreSave(ParamsWriteContext ctx)
+//	{
+//		super.OnStoreSave(ctx);
+//
+//		m_CF_ModStorage.OnStoreSave(ctx);
+//	}
+//
+//	override bool OnStoreLoad(ParamsReadContext ctx, int version)
+//	{
+//		if (!super.OnStoreLoad(ctx, version))
+//		{
+//			return false;
+//		}
+//
+//		return m_CF_ModStorage.OnStoreLoad(ctx, version);
+//	}
 
 	/**
 	 * @brief Refer to CF/ModStorage implementation of ItemBase::CF_OnStoreSave
