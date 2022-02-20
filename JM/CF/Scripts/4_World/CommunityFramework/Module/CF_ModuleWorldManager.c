32316bb19c476e065ca9d5699171fcb06d8c88d2
Jacob <mangosprograms@gmail.com>
2022-02-21 01:29:36 +1100

    Removed arg delete

--- JM/CF/Scripts/4_World/CommunityFramework/Module/CF_ModuleWorldManager.c
+++ JM/CF/Scripts/4_World/CommunityFramework/Module/CF_ModuleWorldManager.c
@@ -15,69 +15,69 @@ class CF_ModuleWorldManager
 	{
 		s_InvokeConnect.OnInvokeConnect(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnInvokeDisconnect(Class sender, CF_EventArgs args)
 	{
 		s_InvokeDisconnect.OnInvokeDisconnect(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnClientNew(Class sender, CF_EventArgs args)
 	{
 		s_ClientNew.OnClientNew(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnClientRespawn(Class sender, CF_EventArgs args)
 	{
 		s_ClientRespawn.OnClientRespawn(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnClientReady(Class sender, CF_EventArgs args)
 	{
 		s_ClientReady.OnClientReady(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnClientPrepare(Class sender, CF_EventArgs args)
 	{
 		s_ClientPrepare.OnClientPrepare(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnClientReconnect(Class sender, CF_EventArgs args)
 	{
 		s_ClientReconnect.OnClientReconnect(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnClientLogout(Class sender, CF_EventArgs args)
 	{
 		s_ClientLogout.OnClientLogout(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnClientDisconnect(Class sender, CF_EventArgs args)
 	{
 		s_ClientDisconnect.OnClientDisconnect(sender, args);
 
-		delete args;
+		//delete args;
 	}
 
 	static void OnClientLogoutCancelled(Class sender, CF_EventArgs args)
 	{
 		s_ClientLogoutCancelled.OnClientLogoutCancelled(sender, args);
 
-		delete args;
+		//delete args;
 	}
 };
