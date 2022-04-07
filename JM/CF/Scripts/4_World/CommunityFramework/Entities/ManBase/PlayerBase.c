bfb2779a4fea4d5837c671edf51808c355aacb96
lava76 <lava76+github@mail.cbw-clan.de>
2022-04-07 22:01:45 +0200

    Make sure player doesn't get a stale queued ID and prefer identity ID

--- JM/CF/Scripts/4_World/CommunityFramework/Entities/ManBase/PlayerBase.c
+++ JM/CF/Scripts/4_World/CommunityFramework/Entities/ManBase/PlayerBase.c
@@ -1,4 +1,3 @@
-#ifdef CF_MODSTORAGE
 modded class PlayerBase
 {
 	protected static ref TStringArray s_CF_QueuedIdentityIDs = new TStringArray();
@@ -18,12 +17,37 @@ modded class PlayerBase
 
 	string CF_GetQueuedIdentityId()
 	{
-		if (!m_CF_QueuedIdentityID)
+		//! Always prefer identity when it is available
+		if (GetIdentity())
 		{
-			m_CF_QueuedIdentityID = CF_DequeueIdentityId();
+			if (m_CF_QueuedIdentityID != GetIdentity().GetId())
+			{
+				CF_Log.Warn("Queued identity ID %1 doesn't match actual ID %2 for player %3", m_CF_QueuedIdentityID, GetIdentity().GetId(), ToString());
+				m_CF_QueuedIdentityID = GetIdentity().GetId();
+			}
+		}
+		else if (!m_CF_QueuedIdentityID && s_CF_QueuedIdentityIDs.Count())
+		{
+			array<PlayerIdentity> identities();
+			GetDayZGame().GetPlayerIndentities(identities);
+			string id;
+			while (s_CF_QueuedIdentityIDs.Count())
+			{
+				id = CF_DequeueIdentityId();
+				if (!id) break;
+				//! Make sure this is not a stale ID. If it's not in the list,
+				//! the player didn't finish connecting (client crash, connection error etc)
+				foreach (PlayerIdentity identity: identities)
+				{
+					if (id == identity.GetId())
+					{
+						m_CF_QueuedIdentityID = id;
+						return m_CF_QueuedIdentityID;
+					}
+				}
+			}
 		}
 
 		return m_CF_QueuedIdentityID;
 	}
 };
-#endif
