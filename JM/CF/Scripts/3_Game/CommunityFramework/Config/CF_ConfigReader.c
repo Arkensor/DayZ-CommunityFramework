f75fef041382e42879ed47494039a8a0bae35934
Wardog <wardog@wrdg.net>
2023-01-29 15:12:31 -0600

    Return CF_Lexer in legacy CF_ConfigReader

--- JM/CF/Scripts/3_Game/CommunityFramework/Config/CF_ConfigReader.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/Config/CF_ConfigReader.c
@@ -2,8 +2,9 @@ typedef CF_ConfigReader ConfigReader;
 
 class CF_ConfigReader : CF_Lexer
 {
+	// use CF_Lexer::Load
 	static CF_Lexer Open(string path, string oldSource = "")
 	{
-		Load(path, oldSource);
+		return Load(path, oldSource);
 	}
 };
