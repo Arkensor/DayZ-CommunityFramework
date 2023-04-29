cec98ac688e8d05baac40f0392995fdf4c1c56f8
Tyler Paul <tylerpaul2329@gmail.com>
2023-04-29 16:24:28 -0400

    Update CF_File.c

--- JM/CF/Scripts/1_Core/CommunityFramework/Files/CF_File.c
+++ JM/CF/Scripts/1_Core/CommunityFramework/Files/CF_File.c
@@ -344,9 +344,7 @@ class CF_File : Managed
 			fileContents += byte.AsciiToString();
 			byte = fileStream.Next();
 		}
-		
-		fileStream.Close();
-		
+				
 		return fileContents;
 	}	
 	
@@ -384,9 +382,7 @@ class CF_File : Managed
 			
 			byte = fileStream.Next();
 		}
-		
-		fileStream.Close();
-		
+				
 		// Add final line to the contents
 		if (fileLine != string.Empty)
 		{
@@ -419,9 +415,7 @@ class CF_File : Managed
 			fileContents.Insert(byte);
 			byte = fileStream.Next();
 		}
-		
-		fileStream.Close();
-		
+				
 		return fileContents;
 	}
 
