b9776ccbf289413d095ef849ba4c68b08ad87961
Wardog <wardog@wrdg.net>
2023-07-09 13:45:02 -0500

    Use Substring over Get for ascii char conversion

--- JM/CF/Scripts/1_Core/CommunityFramework/IO/CF_BinaryWriter.c
+++ JM/CF/Scripts/1_Core/CommunityFramework/IO/CF_BinaryWriter.c
@@ -94,7 +94,7 @@ class CF_BinaryWriter : CF_IO
 		
 		for (int i = 0; i < value.Length(); i++)
 		{
-			m_Stream.AppendCurrent(value[i].ToAscii());
+			m_Stream.AppendCurrent(value.Substring(i, 1).ToAscii());
 		}
 	}
 
