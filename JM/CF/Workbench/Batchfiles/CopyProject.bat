68b7758dd7c10a469ce8973a61db90937226366c
Jacob Kumar <jacob@jacobmango.co>
2024-02-10 23:42:03 +0100

    Updated Batchfiles from COT

--- JM/CF/Workbench/Batchfiles/CopyProject.bat
+++ JM/CF/Workbench/Batchfiles/CopyProject.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enableextensions enabledelayedexpansion
 
 cd /D "%~dp0"
 
@@ -27,23 +28,21 @@ if %failed%==1 (
     goto:eof
 )
 
-set workbenchDirectory=
-set gameDirectory=
-set serverDirectory=
+if exist "%~dp0..\project.cfg.bat" del "%~dp0..\project.cfg.bat"
 
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg WorkbenchDirectory') do (
-    set workbenchDirectory=%%a
+for /f "usebackq delims=" %%a in ( "%~dp0..\project.cfg" ) do (
+	echo set %%a>>"%~dp0..\project.cfg.bat"
 )
 
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg GameDirectory') do (
-    set gameDirectory=%%a
+call "%~dp0..\project.cfg.bat"
+
+if exist "%~dp0..\user.cfg.bat" del "%~dp0..\user.cfg.bat"
+
+for /f "usebackq delims=" %%a in ( "%~dp0..\user.cfg" ) do (
+	echo set %%a>>"%~dp0..\user.cfg.bat"
 )
 
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg ServerDirectory') do (
-    set serverDirectory=%%a
-)
-
-setlocal enableextensions enabledelayedexpansion
+call "%~dp0..\user.cfg.bat"
 
 echo WorkbenchDirectory is: "%workbenchDirectory%"
 if "%workbenchDirectory%"=="" (
--- JM/CF/Workbench/Batchfiles/ZBinarizeDeploy.bat
+++ JM/CF/Workbench/Batchfiles/ZBinarizeDeploy.bat
@@ -1,8 +1,7 @@
 @echo off
+setlocal enableextensions enabledelayedexpansion
 
-cd /D "%~dp0"
-
-set batchDirectory=%cd%
+set batchDirectory=%~dp0
 
 set /a failed=0
 
@@ -35,43 +34,25 @@ if %failed%==1 (
 	goto:eof
 )
 
-set githubDirectory=%cd%\
+set githubDirectory=%~dp0\
 set workbenchDataDirectory=%githubDirectory%Workbench\
 set toolsDirectory=%workbenchDataDirectory%Tools\
 
-set workDrive=
-set modName=
-set modBuildDirectory=
-set prefixLinkRoot=
-set keyDirectory=
-set keyName=
+if exist "%~dp0..\project.cfg.bat" del "%~dp0..\project.cfg.bat"
 
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg WorkDrive') do (
-	set workDrive=%%a
+for /f "usebackq delims=" %%a in ( "%~dp0..\project.cfg" ) do (
+	echo set %%a>>"%~dp0..\project.cfg.bat"
 )
 
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg ModName') do (
-	set modName=%%a
+call "%~dp0..\project.cfg.bat"
+
+if exist "%~dp0..\user.cfg.bat" del "%~dp0..\user.cfg.bat"
+
+for /f "usebackq delims=" %%a in ( "%~dp0..\user.cfg" ) do (
+	echo set %%a>>"%~dp0..\user.cfg.bat"
 )
 
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg ModBuildDirectory') do (
-	set modBuildDirectory=%%a
-)
-
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg PrefixLinkRoot') do (
-	set prefixLinkRoot=%%a
-)
-
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg KeyDirectory') do (
-	set keyDirectory=%%a
-)
-
-for /f "delims=" %%a in ('call ExtractData.bat ../project.cfg ../user.cfg KeyName') do (
-	set keyName=%%a
-)
-
-
-setlocal enableextensions enabledelayedexpansion
+call "%~dp0..\user.cfg.bat"
 
 echo KeyDirectory is: "%keyDirectory%"
 if "%keyDirectory%"=="" (
@@ -165,6 +146,40 @@ IF NOT exist "%modBuildDirectory%%modName%\Keys\" (
 echo Copying over "%workDrive%%prefixLinkRoot%\mod.cpp" to "%modBuildDirectory%%modName%\"
 copy "%workDrive%%prefixLinkRoot%\mod.cpp" "%modBuildDirectory%%modName%\" > nul
 
+for /F "tokens=*" %%F in ('git rev-parse --abbrev-ref HEAD') do (
+	set branch=%%F
+)
+echo GIT branch: %branch%
+
+REM Base timestamp (in seconds)
+REM Add UNIX timestamp to this and multiply by 1e7 to match what DayZ publishing tools would produce
+set timestamp=523304198640
+
+call :UnixTime
+
+echo Creating "%modBuildDirectory%%modName%\meta.cpp"
+if exist "%modBuildDirectory%%modName%\meta.cpp" del /F "%modBuildDirectory%%modName%\meta.cpp"
+for /f "usebackq tokens=1,2 delims==;" %%a in ( "%~dp0..\..\meta.%branch%.cpp" ) do (
+	set key=%%a
+	set key=!key: =!
+	set value=%%b
+	set value=!value: =!
+	if !key!==timestamp (
+		REM batch only supports 32-bit numbers, so we have to split into several operations
+		set /a value1=!timestamp:~0,5!+!ss:~0,3!
+		set /a value2=!timestamp:~5,7!+!ss:~3,7!
+		if !value2! GTR 9999999 (
+			set /a value1=!value1!+1
+			set value2=!value2:~1,7!
+		)
+		REM Make sure we keep leading zeros of ms value by prepending a digit before adding, then discarding it
+		set /a value3=10000000+!ms!*100
+		set value=!value1!!value2!!value3:~1!
+	)
+	echo !key! = !value!;>>"%modBuildDirectory%%modName%\meta.cpp"
+)
+type "%modBuildDirectory%%modName%\meta.cpp"
+
 echo Copying over "%keyDirectory%\%keyName%.bikey" to "%modBuildDirectory%%modName%\Keys\"
 echo Copying over "%keyDirectory%\%keyName%.biprivatekey" to "%modBuildDirectory%%modName%\Keys\"
 
@@ -172,7 +187,7 @@ echo Packaging %modName% PBO's
 
 @echo off
 
-cd /D "%workDrive%%prefixLinkRoot%\"
+pushd "%workDrive%%prefixLinkRoot%\"
 
 for /R %%D in ( config.cpp ) do (
 	echo Checking directory %%~dD%%~pD, searching for config.cpp
@@ -187,8 +202,8 @@ for /R %%D in ( config.cpp ) do (
 								IF NOT EXIST "%%~dD%%~pD..\..\..\..\..\..\..\config.cpp" (
 									IF NOT EXIST "%%~dD%%~pD..\..\..\..\..\..\..\..\config.cpp" (
 										rem echo No parent config.cpp found, building pbo %%D
-										echo START /W "BinarizePBO" "%batchDirectory%/BinarizePBO.bat" %%D fuckThurston %compression%
-										START /W "BinarizePBO" "%batchDirectory%/BinarizePBO.bat" %%D fuckThurston %compression%
+										echo START /MIN "BinarizePBO" "%batchDirectory%/BinarizePBO.bat" %%D fuckThurston %compression%
+										START /MIN "BinarizePBO" "%batchDirectory%/BinarizePBO.bat" %%D fuckThurston %compression%
 									)
 								)
 							)
@@ -200,7 +215,19 @@ for /R %%D in ( config.cpp ) do (
 	)
 )
 
-goto end
+popd
 
-:end
-endlocal
+exit /b
+
+:UnixTime
+for /F "tokens=2,3,4 delims==.+" %%t in ('%SystemRoot%\System32\wbem\wmic.exe OS GET LocalDateTime /VALUE') do set "ts=%%t.%%u+%%v"
+set /a "yy=10000%ts:~0,4% %% 10000, mm=100%ts:~4,2% %% 100, dd=100%ts:~6,2% %% 100"
+set /a "dd=dd-2472663+1461*(yy+4800+(mm-14)/12)/4+367*(mm-2-(mm-14)/12*12)/12-3*((yy+4900+(mm-14)/12)/100)/4"
+set /a ss=(((1%ts:~8,2%*60)+1%ts:~10,2%)*60)+1%ts:~12,2%-366100-%ts:~21,1%((1%ts:~22,3%*60)-60000)
+set /a ss+=dd*86400
+set ms=%ts:~15,5%
+echo %ss%.%ms%
+:StripLeadingZeros
+if %ms:~0,1%==0 set ms=%ms:~1%
+if %ms:~0,1%==0 goto StripLeadingZeros
+exit /b
